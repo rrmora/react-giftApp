7eb44b884a86550fbc86af086efbf08a8385305b
Raul Reyes <raul.remo02@gmail.com>
2020-08-11 19:53:11 -0500

    docs creado

---
+++ docs/precache-manifest.a346e4738a86c486e195cb272e4d59ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057a5142ab925f376fb83d36abec2464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d9b89c4cc4b19fa13c",
+    "url": "/static/css/main.77cfb72d.chunk.css"
+  },
+  {
+    "revision": "84afe8cb2dde5ab88221",
+    "url": "/static/js/2.b48d0b2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b48d0b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d9b89c4cc4b19fa13c",
+    "url": "/static/js/main.e6d002e1.chunk.js"
+  },
+  {
+    "revision": "28b2448ea3201b2f979f",
+    "url": "/static/js/runtime-main.09460a5c.js"
+  }
+]);
